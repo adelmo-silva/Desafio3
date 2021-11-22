625bbe2e844da1c67aa776484c8afa5e22120819
Adelmo Silva <adelmotocomsono@gmail.com>
2021-11-22 02:33:07 -0300

    Única versão

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿{
+    Console.Write("Informe um número: ");
+    double número = double.Parse(Console.ReadLine());
+    
+    if(número != 0)
+    {
+        Console.Write("Informe um número novamente: ");
+        double númeroplus = double.Parse(Console.ReadLine());
+        double result = número + númeroplus;
+        Console.WriteLine("O resultado é " + result);
+
+        while(númeroplus != 0)
+        {
+            Console.Write("Informe um número novamente: ");
+            númeroplus = double.Parse(Console.ReadLine());
+            result = result + númeroplus;
+            
+            if(númeroplus != 0)
+            {
+            Console.WriteLine("O resultado é " + result);
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,1 @@
+Crie um algoritmo para somar e exibir os valores que o usuário informar, porém, quando ele digitar o número zero, o loop deve acabar.
